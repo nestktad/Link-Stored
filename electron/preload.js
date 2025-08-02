972edef2d060b99f01ae4312d6c03bcc15a522bf
dat <datchthk22@gmail.com>
2025-08-02 17:28:29 +0700

    Add electron to run app

--- electron/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    saveFile: (data) => ipcRenderer.invoke('save-file', data),
-    readFile: () => ipcRenderer.invoke('read-file'),
-});
